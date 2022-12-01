4e0df69019856fbecd535be19e9b9bde3450fc65
hogehogeBrav <zmzmakoto@icloud.com>
2022-12-01 13:55:43 +0900

    [add] U_sales_lists

--- index.js
+++ index.js
@@ -166,26 +166,27 @@ app.get('/auction/:auction_ID', isAuthenticated, (req, res) => {
         ON (auction_bid.user_ID = user.user_ID) 
         WHERE auction_ID = ` + req.params.auction_ID + ` 
         ORDER BY amount_time ASC;`,
-      (error2, results2) => {
-        console.log(results2);
-        if (error2) {
-          console.log('error connecting: ' + error2.stack);
-          res.status(400).send({ message: 'Error!!' });
-          return;
+        (error2, results2) => {
+          console.log(results2);
+          if (error2) {
+            console.log('error connecting: ' + error2.stack);
+            res.status(400).send({ message: 'Error!!' });
+            return;
+          }
+          // 入札金額
+          var now_amount = results[0].max_amount;
+          if(results[0].max_amount == null){
+            now_amount = results[0].minimum_amount;
+          }
+          res.render('U_auction_room.ejs', {
+            auction: results,
+            auction_bid_history: results2,
+            now_amount: now_amount,
+            id: req.user.user_ID,
+            name: req.user.name,
+          });
         }
-        // 入札金額
-        var now_amount = results[0].max_amount;
-        if(results[0].max_amount == null){
-          now_amount = results[0].minimum_amount;
-        }
-        res.render('U_auction_room.ejs', {
-          auction: results,
-          auction_bid_history: results2,
-          now_amount: now_amount,
-          id: req.user.user_ID,
-          name: req.user.name,
-        });
-      });
+      );
     }
   );
 });
@@ -209,6 +210,56 @@ app.post('/auction', isAuthenticated, (req, res) => {
         return;
       }
       res.write(JSON.stringify({ message: 'Success!!' }));
+    }
+  );
+});
+
+app.get('/sales', (req, res)=> {
+  connection.query(
+    `SELECT s.sales_ID, s.bid_price, s.bid_date, s.sales_status_ID, ss.state, u.name as user_name, m.name as model_name 
+    FROM sales as s 
+    INNER JOIN sales_status as ss 
+    ON s.sales_status_ID = ss.sales_status_ID 
+    INNER JOIN user as u 
+    ON s.user_ID = u.user_ID 
+    INNER JOIN stock as st 
+    ON s.car_ID = st.car_ID 
+    INNER JOIN model as m 
+    ON st.car_model_ID = m.car_model_ID 
+    ORDER BY sales_ID DESC`,
+    (error, results) => {
+        if(error) {
+            console.log('error conenction: ' + error.stack);
+            return;
+        }
+
+        for(let i=0; i < results.length; i++){
+            // デフォルト値
+            let format = 'YYYY年MM月DD日 hh:mm';
+
+            format = format.replace(/YYYY/g, results[i].bid_date.getFullYear());
+            format = format.replace(/MM/g, ('0' + (results[i].bid_date.getMonth() + 1)).slice(-2));
+            format = format.replace(/DD/g, ('0' + results[i].bid_date.getDate()).slice(-2));
+            format = format.replace(/hh/g, ('0' + results[i].bid_date.getHours()).slice(-2));
+            format = format.replace(/mm/g, ('0' + results[i].bid_date.getMinutes()).slice(-2));
+
+            results[i].bid_date = format;
+        }
+
+
+        connection.query(
+            'SELECT * FROM sales_status',
+            (error, options) => {
+                if(error) {
+                    console.log('error conenction: ' + error.stack);
+                    return; 
+                }
+                
+                let json_result = JSON.stringify(results);
+                let json_option = JSON.stringify(options);
+                res.render('A_sales_lists.ejs', {values:results, options:options, json_result:json_result, json_option:json_option});
+            }
+        );
     }
   );
 });
