c73ed8c4b29c95084fa3896d3117439d2720ae84
komaki216 <komaki.0025@gmail.com>
2022-12-12 14:24:02 +0900

    [update]車両詳細情報表示部分作成

--- A_car_detail.js
+++ A_car_detail.js
@@ -6,7 +6,7 @@
  */
 
 // 車両情報SQL文
-let sql = 'SELECT stock.* , maker.maker_name AS maker_name, model.name AS model_name, run_status.run_status AS run_status, color.color_name AS color_category, body_type.body_type_name AS body_type, car_detail.*, option.*';
+let sql = 'SELECT stock.* , maker.maker_name AS maker_name, model.name AS model_name, run_status.run_status AS run_status, color.color_name AS color_category, body_type.body_type_name AS body_type, car_detail.*, option.*, drive.drive AS drive, fuel.fuel AS fuel, car_history.car_history AS car_history';
 sql += ' FROM stock';
 sql += ' INNER JOIN maker ON stock.maker_ID = maker.maker_ID';// メーカー名
 sql += ' INNER JOIN model ON stock.car_model_ID = model.car_model_ID AND stock.maker_ID = model.maker_ID';//車種
@@ -15,6 +15,9 @@ sql += ' INNER JOIN color ON stock.color_ID = color.color_ID';//色
 sql += ' INNER JOIN body_type ON stock.body_type_ID = body_type.body_type_ID';//ボディタイプ
 sql += ' INNER JOIN car_detail ON stock.car_ID = car_detail.car_ID';//車両詳細
 sql += ' INNER JOIN option ON stock.car_ID = option.car_ID';//装備品
+sql += ' INNER JOIN drive ON car_detail.drive_ID = drive.drive_ID';//駆動方式
+sql += ' INNER JOIN fuel ON car_detail.fuel_ID = fuel.fuel_ID';//燃料
+sql += ' INNER JOIN car_history ON car_detail.car_history_ID = car_history.car_history_ID';//車歴
 sql += ' WHERE stock.car_ID = ?';
 sql += ';'
 
