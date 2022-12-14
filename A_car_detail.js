62b26de99ddda1640108695ec8c1afddf1d68939
komaki216 <komaki.0025@gmail.com>
2022-12-14 14:24:46 +0900

    [update]編集モーダルデータ表示

--- A_car_detail.js
+++ A_car_detail.js
@@ -21,13 +21,61 @@ sql += ' INNER JOIN car_history ON car_detail.car_history_ID = car_history.car_h
 sql += ' WHERE stock.car_ID = ?';
 sql += ';'
 
+const maker_sql = 'SELECT * FROM maker;';
+const model_sql = 'SELECT * FROM model;';
+const run_status_sql = 'SELECT * FROM run_status;';
+const color_sql = 'SELECT * FROM color;';
+const body_sql = 'SELECT * FROM body_type;';
+const drive_sql = 'SELECT * FROM drive;';
+const fuel_sql = 'SELECT * FROM fuel';
+const car_history_sql = 'SELECT * FROM car_history';
+
 exports.main = async function(con,req,res) {
+  // 編集モーダル用情報
+  let stock_list = await findAll(con,sql);
+  let maker_list = await findAll(con,maker_sql);
+  let model_list = await findAll(con,model_sql);
+  let run_status_list = await findAll(con,run_status_sql);
+  let color_list = await findAll(con,color_sql);
+  let body_list = await findAll(con,body_sql);
+  let drive_list = await findAll(con,drive_sql);
+  let fuel_list = await findAll(con,fuel_sql);
+  let car_history_list = await findAll(con,car_history_sql);
+  //車両詳細情報
   let car_info = await selectCar(con,sql,req.params.car_ID);
   console.log(car_info);
   //車両詳細画面を呼出
   res.render('A_car_detail.ejs', {
-    car_info: car_info
+    stock_list: stock_list,             //在庫全件
+    maker_list: maker_list,             //メーカー全件
+    model_list: model_list,             //車種全件
+    run_status_list: run_status_list,   //走行状態
+    color_list: color_list,             //色系統
+    body_list: body_list,               //ボディタイプ
+    drive_list: drive_list,             //駆動方式
+    fuel_list: fuel_list,               //燃料
+    car_history_list: car_history_list, //車歴
+    car_info: car_info                  //車両詳細
   });
+}
+
+/**
+ * 全件取得用メソッド
+ * @param {*} con DBコネクション
+ * @param {*} sql SQL文
+ * @returns 取得したデータ
+ */
+async function findAll (con,sql) {
+  const data = await new Promise((resolve, reject) => {
+    con.query(sql,
+      (error, results) => {
+        resolve({
+          error: error,
+          results: results
+        });
+      });
+    })
+  return data.results;
 }
 
 /**
