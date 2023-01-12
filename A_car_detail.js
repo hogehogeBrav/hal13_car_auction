fc802102faad44e679f2cc850c6b661041e0001b
komaki216 <komaki.0025@gmail.com>
2023-01-12 14:39:38 +0900

    [update]車両情報編集機能作成

--- A_car_detail.js
+++ A_car_detail.js
@@ -30,6 +30,13 @@ const drive_sql = 'SELECT * FROM drive;';
 const fuel_sql = 'SELECT * FROM fuel';
 const car_history_sql = 'SELECT * FROM car_history';
 
+// 編集用SQL
+const update_stock_sql = "UPDATE stock SET maker_ID = ?, car_model_ID = ?, grade = ?, displacement = ?, model_year = ?, import_model_year = ?, mileage = ?, run_status_ID = ?, color_ID = ?, color_name = ?, insurance_deadline = ?, mt_at = ?, body_type_ID = ?, car_type = ? WHERE car_ID = ?;";
+const update_detail_sql ="UPDATE car_detail SET door = ?, ride_member = ?, drive_ID = ?, fuel_ID = ?, checking_memory = ?, repair_history = ?, car_number = ?, recycling_deposit = ?, import_route = ?, handle = ?, car_history_ID = ?, owner_history = ?, delivery_condition = ? WHERE car_ID = ?;";//先頭のcar_IDを削除
+const update_option_sql ="UPDATE option SET air_conditioner = ?, smart_key = ?, sun_roof = ?, low_down = ?, power_steering = ?, cd = ?, leather_seat = ?, non_smoking = ?, power_window = ?, md = ?, aero_parts = ?, pet = ?, central_door_lock = ?, dvd = ?, genuine_alminium_wheel = ?, limited_edition = ?, abs = ?, tv = ?, skid_prevention = ? ,test_drive = ?, airbag = ?, navi = ?, traction_control = ?, manual = ?, etc = ? ,back_cam = ?, cold_climate = ?, warranty = ?, key_less = ?, electric_door = ?, welfare_vechicles = ?, spare_tire = ? WHERE car_ID = ?;";
+// const update_option_sql ="UPDATE option SET ? WHERE car_ID = ?;";
+
+
 exports.main = async function(con,req,res) {
   // 編集モーダル用情報
   let stock_list = await findAll(con,sql);
@@ -57,6 +64,96 @@ exports.main = async function(con,req,res) {
     car_history_list: car_history_list, //車歴
     car_info: car_info                  //車両詳細
   });
+}
+
+// 編集されたときの処理
+exports.update = async function(con,req,res){
+  let stock_values = [
+    req.body.maker_id, //select
+    req.body.model_id, //select
+    req.body.grade, //text
+    req.body.displacement, //text
+    req.body.model_year, //text
+    req.body.import_model_year, //text
+    req.body.mileage, //text
+    req.body.runstatus_id, //select
+    req.body.color_type, //select
+    req.body.color_name, //text
+    req.body.insurance_deadline, //text
+    req.body.AT_MT, //select
+    req.body.body_type_id, //select
+    req.body.car_type, //text
+    req.params.car_ID //get
+  ];
+  let detail_values = [
+    req.body.door,                //ドア数
+    req.body.ride_member,         //乗車人数
+    req.body.drive_id,            //駆動方式ID
+    req.body.fuel_id,             //燃料ID
+    req.body.check_memo,          //点検記録簿
+    req.body.repair,              //修復歴
+    req.body.car_number,          //車台番号
+    req.body.recycle,             //リサイクル預託金
+    req.body.route,               //輸入経路
+    req.body.handle,              //ハンドル
+    req.body.car_history_id,      //車歴ID
+    req.body.owner_history,       //所有車歴
+    req.body.delivery_condition,   //引き渡し条件
+    req.params.car_ID
+  ];
+  let option_values = [
+    0,//air_conditioner //0
+    0,// smart_key //1
+    0,// sun_roof //2
+    0,// low_down //3
+    0,// power_steering //4
+    0,// cd //5
+    0,// leather_seat //6
+    0,// non_smoking //7
+    0,// power_window //8
+    0,// md //9
+    0,// aero_parts //10
+    0,// pet //11
+    0,// central_door_lock //12
+    0,// dvd //13
+    0,// genuine_alminium_wheel //14
+    0,// limited_edition //15
+    0,// abs //16
+    0,// tv //17
+    0,// skid_prevention //18
+    0,// test_drive //19
+    0,// airbag //20
+    0,// navi //21
+    0,// traction_control //22
+    0,// manual //23
+    0,// etc //24
+    0,// back_cam //25
+    0,// cold_climate //26 
+    0,// warranty //27
+    0,// key_less //28
+    0,// electric_door //29
+    0,// welfare_vechicles //30
+    0,// spare_tire //31
+    req.params.car_ID //32
+  ];
+  // チェックボックスの選択を配列に変換
+  for(var key in option_values) {
+    if(Array.isArray(req.body.parts)){
+      for(var i=0; i<req.body.parts.length; i++){
+        // console.log("key:" + key + "req:" + req.body.parts[i]);
+        if(key == req.body.parts[i]){
+          option_values[key] = 1;
+        }
+      }
+    // } else if(key == req.body.parts){
+    //   option_values[req.body.parts] = 1;
+    }
+  }
+  console.log(option_values);
+  // DB更新
+  await updateCar(con,update_stock_sql,stock_values);
+  await updateCar(con,update_detail_sql,detail_values);
+  await updateCar(con,update_option_sql,option_values,req.params.car_ID);
 }
 
 /**
@@ -98,47 +195,31 @@ async function selectCar (con,sql,carId) {
   return data.results[0];
 }
 
-// 編集機能追加予定
-const update_stock_sql = "UPDATE stock SET maker_ID = ?, car_model_ID = ?, grade = ?, displacement = ?, model_year = ?, import_model_year = ?, mileage = ?, run_status_ID = ?, color_ID = ?, color_name = ?, insurance_deadline = ?, mt_at = ?, body_type_ID = ?, car_type = ? WHERE car_ID = ?;";
-const update_detail_sql ="UPDATE car_detail SET door = ?, ride_member = ?, drive_ID = ?, fuel_ID = ?, checking_memory = ?, repair_history = ?, car_number = ?, recycling_deposit = ?, import_route = ?, handle = ?, car_history_ID = ?, owner_history = ?, delivery_condition = ? WHERE car_ID = ?;";//先頭のcar_IDを削除
-const update_option_sql ="UPDATE option SET air_conditioner = ?, smart_key = ?, sun_roof = ?, low_down = ?, power_steering = ?, cd = ?, leather_seat = ?, non_smoking = ?, power_window = ?, md = ?, aero_parts = ?, pet = ?, central_door_lock = ?, dvd = ?, genuine_alminium_wheel = ?, limited_edition = ?, abs = ?, tv = ?, skid_prevention = ? ,test_drive = ?, airbag = ?, navi = ?, traction_control = ?, manual = ?, etc = ? ,back_cam = ?, cold_climate = ?, warranty = ?, key_less = ?, electric_door = ?, welfare_vechicles = ?, spare_tire = ? WHERE car_ID = ? ;";
-function updateCar(con,sql,carId) {
-
+/**
+ * 車両情報編集用メソッド
+ * @param {*} con DBコネクション
+ * @param {*} sql SQL文
+ * @param {*} values 
+ * @returns 取得したデータ
+ */
+async function updateCar(con,sql,values) {
+  const data = await new Promise((resolve, reject) => {
+    con.query(sql, values,
+      (error, results) => {
+        resolve({
+          error: error,
+          results: results
+        });
+        if (error) {
+          console.log('error connecting: ' + error.stack);
+          res.status(400).send({ message: 'Error!!' });
+          return;
+        }
+        console.log('在庫更新完了');
+      }
+    );
+  })
+  return data.results.insertId;
 }
-
-let option_values = {
-  // air_conditioner: 0,//2
-  // smart_key: 0,//3
-  // sun_roof:0 ,//4
-  // low_down: 0,//5
-  // power_steering: 0,//6
-  // cd: 0,//7
-  // leather_seat: 0,//8
-  // non_smoking: 0,//9
-  // power_window: 0,//10
-  // md: 0,//11
-  // aero_parts: 0, //12
-  // pet: 0,//13
-  // central_door_lock: 0,//14
-  // dvd: 0,//15
-  // genuine_alminium_wheel: 0,//16
-limited_edition: 0,//17
-abs: 0,//18
-tv: 0,//19
-skid_prevention: 0,//20
-test_drive: 0,//21
-airbag: 0,//22
-navi: 0,//23
-traction_control: 0,//24
-manual: 0,//25
-etc: 0,//26
-back_cam: 0,//27
-cold_climate: 0,//28 
-warranty: 0,//29
-key_less: 0,//30
-electric_door: 0,//31
-welfare_vechicles: 0,//32
-spare_tire: 0//33
-                                                                                                                              };
 
 // 削除機能追加予定
