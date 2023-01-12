0f6931a876e719fd7e882e21bc0cbb5221f5030c
komaki216 <komaki.0025@gmail.com>
2023-01-12 12:46:05 +0900

    [update]編集用SQL文作成

--- A_car_detail.js
+++ A_car_detail.js
@@ -97,3 +97,48 @@ async function selectCar (con,sql,carId) {
   })
   return data.results[0];
 }
+
+// 編集機能追加予定
+const update_stock_sql = "UPDATE stock SET maker_ID = ?, car_model_ID = ?, grade = ?, displacement = ?, model_year = ?, import_model_year = ?, mileage = ?, run_status_ID = ?, color_ID = ?, color_name = ?, insurance_deadline = ?, mt_at = ?, body_type_ID = ?, car_type = ? WHERE car_ID = ?;";
+const update_detail_sql ="UPDATE car_detail SET door = ?, ride_member = ?, drive_ID = ?, fuel_ID = ?, checking_memory = ?, repair_history = ?, car_number = ?, recycling_deposit = ?, import_route = ?, handle = ?, car_history_ID = ?, owner_history = ?, delivery_condition = ? WHERE car_ID = ?;";//先頭のcar_IDを削除
+const update_option_sql ="UPDATE option SET air_conditioner = ?, smart_key = ?, sun_roof = ?, low_down = ?, power_steering = ?, cd = ?, leather_seat = ?, non_smoking = ?, power_window = ?, md = ?, aero_parts = ?, pet = ?, central_door_lock = ?, dvd = ?, genuine_alminium_wheel = ?, limited_edition = ?, abs = ?, tv = ?, skid_prevention = ? ,test_drive = ?, airbag = ?, navi = ?, traction_control = ?, manual = ?, etc = ? ,back_cam = ?, cold_climate = ?, warranty = ?, key_less = ?, electric_door = ?, welfare_vechicles = ?, spare_tire = ? WHERE car_ID = ? ;";
+function updateCar(con,sql,carId) {
+
+}
+
+let option_values = {
+  // air_conditioner: 0,//2
+  // smart_key: 0,//3
+  // sun_roof:0 ,//4
+  // low_down: 0,//5
+  // power_steering: 0,//6
+  // cd: 0,//7
+  // leather_seat: 0,//8
+  // non_smoking: 0,//9
+  // power_window: 0,//10
+  // md: 0,//11
+  // aero_parts: 0, //12
+  // pet: 0,//13
+  // central_door_lock: 0,//14
+  // dvd: 0,//15
+  // genuine_alminium_wheel: 0,//16
+limited_edition: 0,//17
+abs: 0,//18
+tv: 0,//19
+skid_prevention: 0,//20
+test_drive: 0,//21
+airbag: 0,//22
+navi: 0,//23
+traction_control: 0,//24
+manual: 0,//25
+etc: 0,//26
+back_cam: 0,//27
+cold_climate: 0,//28 
+warranty: 0,//29
+key_less: 0,//30
+electric_door: 0,//31
+welfare_vechicles: 0,//32
+spare_tire: 0//33
+                                                                                                                              };
+
+// 削除機能追加予定
